cd90125d55628e458443ca9232afbd0ef11211ab
MrDexII <free.andrew.a@gmail.com>
2021-05-25 20:12:37 +0200

    initial commit

---
+++ src/main/java/com/andrew/springcloudserverexample/SpringCloudServerExampleApplication.java
@@ -0,0 +1,15 @@
+package com.andrew.springcloudserverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerExampleApplication.class, args);
+	}
+
+}
